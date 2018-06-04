a25c13be1878cd9d4f477c747922a78452c48eb5
martingasparstraus <martin.straus@fit.com.ar>
2018-06-04 19:36:18 -0300

    Se agrega inicialización de usuario administrador.

--- hubble.backend.api/src/main/java/hubble/backend/api/HubbleBackendAPIApplication.java
+++ hubble.backend.api/src/main/java/hubble/backend/api/HubbleBackendAPIApplication.java
@@ -1,5 +1,6 @@
 package hubble.backend.api;
 
+import hubble.backend.business.services.interfaces.services.InitialDataService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
@@ -11,6 +12,6 @@ public class HubbleBackendAPIApplication {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(HubbleBackendAPIApplication.class, args);
-
+        ctx.getBean(InitialDataService.class).createData();
     }
 }
