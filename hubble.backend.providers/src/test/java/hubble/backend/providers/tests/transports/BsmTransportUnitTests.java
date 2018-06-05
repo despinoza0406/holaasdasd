c0fda6583b2f88e3226434a40dbf0aab12450510
tsoftdevscore <andre.vigneaux@tsoftglobal.com>
2018-06-05 13:22:48 -0300

    correccion BSM para que guarde applicationId

--- hubble.backend.providers/src/test/java/hubble/backend/providers/tests/transports/BsmTransportUnitTests.java
+++ hubble.backend.providers/src/test/java/hubble/backend/providers/tests/transports/BsmTransportUnitTests.java
@@ -1,7 +1,28 @@
 package hubble.backend.providers.tests.transports;
 
+import hubble.backend.providers.configurations.ProvidersConfiguration;
+import hubble.backend.providers.transports.implementations.bsm.BsmTransportImpl;
+import hubble.backend.providers.transports.interfaces.BsmTransport;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.xml.soap.SOAPBody;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ProvidersConfiguration.class)
 public class BsmTransportUnitTests {
-    public void BsmTransport_should_get_data_from_bsm(){
-       // TODO
+    @Autowired
+    BsmTransportImpl bsmTransport;
+
+    @Test
+    public void BsmTransport_should_get_applications_from_bsm(){
+        SOAPBody data = bsmTransport.getApplications();
+
+        assertNotNull(data);
     }
 }
