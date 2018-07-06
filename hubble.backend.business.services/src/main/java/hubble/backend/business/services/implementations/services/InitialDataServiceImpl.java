1577451cde82313361f4c127aeb25f2c660269cd
Daniel Espinoza <daniel.espinoza@tsoftlatam.com>
2018-07-06 14:48:17 -0300

    Se crearon las aplicaciones CRM, Benchmark Home Banking y Benchmark Mobile a la inicializacion de aplicaciones

--- hubble.backend.business.services/src/main/java/hubble/backend/business/services/implementations/services/InitialDataServiceImpl.java
+++ hubble.backend.business.services/src/main/java/hubble/backend/business/services/implementations/services/InitialDataServiceImpl.java
@@ -73,12 +73,12 @@ public class InitialDataServiceImpl implements InitialDataService {
     }
 
     private void configureApplications() {
-        guardarAplicacion("home-banking", "Home Banking", "Home Banking", "Home Banking", "HB", "Home Banking");
-        guardarAplicacion("Benchmark Home Banking", "Home Banking", "Home Banking", "Home Banking", "HB", "Home Banking");
-        guardarAplicacion("mobile-banking", "Mobile Banking", "Plan 17 Eje Ingresos Sub Eje Individuos", "Mobile", "MB", "Mobile Banking");
-        guardarAplicacion("Benchmark Mobile","Mobile Banking", "Plan 17 Eje Ingresos Sub Eje Individuos", "Mobile", "MB", "Mobile Banking");
-        guardarAplicacion("crm", "CRM", "Retail banking", "CRM", "CRM", "CRM");
-        guardarAplicacion("CRM","CRM", "Retail banking", "CRM", "CRM","CRM");
+        guardarAplicacion("home-banking", "Home Banking","aplicacion home-banking", "Home Banking", "Home Banking", "HB", "Home Banking");
+        guardarAplicacion("Benchmark Home Banking", "Home Banking","aplicacion Benchmark Home Banking", "Home Banking", "Home Banking", "HB", "Home Banking");
+        guardarAplicacion("mobile-banking", "Mobile Banking","aplicacion mobile-banking", "Plan 17 Eje Ingresos Sub Eje Individuos", "Mobile", "MB", "Mobile Banking");
+        guardarAplicacion("Benchmark Mobile","Mobile Banking","aplicacion Benchmark Mobile", "Plan 17 Eje Ingresos Sub Eje Individuos", "Mobile", "MB", "Mobile Banking");
+        guardarAplicacion("crm", "CRM","Aplicacion crm", "Retail banking", "CRM", "CRM", "CRM");
+        guardarAplicacion("CRM","CRM","Aplicacion CRM", "Retail banking", "CRM", "CRM","CRM");
     }
 
     private void guardarAplicacion(String id, String nombre, String descripcion, String nombreEnPPM, String nombreEnALM, String nombreEnJira, String nombreEnSiteScope) {
