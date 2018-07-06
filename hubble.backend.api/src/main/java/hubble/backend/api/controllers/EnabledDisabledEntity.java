50586bea9ba6d25ce16909459df04311a9e5db62
guelmy.diaz.blanco@fit.com.ar <guelmy.diaz.blanco@fit.com.ar>
2018-07-06 15:20:09 -0300

    Entity para Enabled/Disabled

---
+++ hubble.backend.api/src/main/java/hubble/backend/api/controllers/EnabledDisabledEntity.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hubble.backend.api.controllers;
+
+/**
+ *
+ * @author Guelmy
+ */
+public class EnabledDisabledEntity {
+    
+    private String id;
+    private boolean enabled;
+
+    public EnabledDisabledEntity() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+    
+    
+    
+}
